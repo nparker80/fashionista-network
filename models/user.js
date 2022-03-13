3ba230f43a7b4ab80fe790e4111a74034b246a55
Najla Parker <najla08@gmail.com>
2022-03-13 14:24:56 -0600

    Added moment package and created user model

---
+++ models/user.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  username: {
+    type: 'String',
+    required: true,
+    unique: true,
+    trim: true
+  },
+
+  email: { 
+    type: 'String',
+    required: true,
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Use a valid E-mail address.",
+    ],
+  },
+
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought',
+    },
+  ],
+
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ]
+},
+{
+  toJSON: {
+    virtuals: true,
+    getters: true,
+  },
+  id:false
+});
+
+userSchema.virtual("friendCount").get(function() {
+  return this.friends.length;
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
